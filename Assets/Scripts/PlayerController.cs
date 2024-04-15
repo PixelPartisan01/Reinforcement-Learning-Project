254ef63cbee4e6df7fa8f76df520ab0ea6dc7ab9
Váradi Sándor <varadisandor011@gmail.com>
2024-04-15 19:36:04 +0200

    -Fixed: player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,30 +1,34 @@
+using Unity.MLAgents.Actuators;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Unity.MLAgents;
-using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 using UnityEngine;
 
 public class PlayerController : Agent
 {
-    public float speed = 5f;
+    public float speed = 1;
+    public float rotationSpeed = 1.0f;
+
+    public bool buttonPressed = false;
 
     public Transform TargetTransform;
 
     public Animator animator;
 
-    private enum ACTIONS
-    {
-        LEFT = 0,
-        FORWARD = 1,
-        RIGHT = 2,
-        BACKWARD = 3
-    }
+    //private enum ACTIONS
+    //{
+    //    LEFT = 0,
+    //    FORWARD = 1,
+    //    RIGHT = 2,
+    //    BACKWARD = 3
+    //}
 
     public override void OnEpisodeBegin()
     {
+        
         transform.localPosition = new Vector3(0, 0.5f, 0);
         animator = GetComponent<Animator>();
 
@@ -32,10 +36,9 @@ public class PlayerController : Agent
         //float xPosition = UnityEngine.Random.Range(-9, 9);
         //float zPosition = UnityEngine.Random.Range(-9, 9);
 
-
-
-        // Assign the randomly generated position to the treasure prefab
         TargetTransform.localPosition = new Vector3(25.0f, 0.0f, 3.0f);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, defaultRotation, rotationSpeed * Time.deltaTime);
+        transform.rotation.Set(0.0f, -90.0f, 0.0f, 0.0f);
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -54,59 +57,68 @@ public class PlayerController : Agent
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        ActionSegment<int> actions = actionsOut.DiscreteActions;
 
-        var horizontal = Input.GetAxisRaw("Horizontal");
-        var vertical = Input.GetAxisRaw("Vertical");
+        ActionSegment<float> actions = actionsOut.ContinuousActions;
 
-        if (horizontal == +1)
+        actions[0] = -1;
+        actions[1] = 0;
+
+        if (Input.GetKey("w"))
         {
-            actions[0] = (int)ACTIONS.LEFT;
+            actions[0] = 1;
+            buttonPressed = true;
+            animator.SetBool("walk", true);
         }
-        else if (horizontal == -1)
+        if (Input.GetKey("d"))
         {
-            actions[0] = (int)ACTIONS.RIGHT;
+            actions[1] = +rotationSpeed;
+            buttonPressed = true;
+            //animator.SetBool("walk", true);
         }
-        else if (vertical == +1)
+        if (Input.GetKey("a"))
         {
-            actions[0] = (int)ACTIONS.FORWARD;
+            actions[1] = -rotationSpeed;
+            buttonPressed = true;
+            //animator.SetBool("walk", true);
         }
-        else if (vertical == -1)
+
+        if(!Input.anyKey)
         {
-            actions[0] = (int)ACTIONS.BACKWARD;
+            buttonPressed = false;
+            animator.SetBool("walk", false);
         }
-        else
-        {
-            actions[0] = 3;
-        }
+        
     }
-
+    
     public override void OnActionReceived(ActionBuffers actions)
     {
-        var actionTaken = actions.DiscreteActions[0];
-        
+        var actionTaken = actions.ContinuousActions;
 
-        switch (actionTaken)
-        {
-            case (int)ACTIONS.FORWARD:
-                transform.rotation = Quaternion.Euler(0, 0, 0);
-                animator.SetBool("walk", true);
-                break;
-            case (int)ACTIONS.LEFT:
-                transform.rotation = Quaternion.Euler(0, -90, 0);
-                animator.SetBool("walk", true);
-                break;
-            case (int)ACTIONS.RIGHT:
-                transform.rotation = Quaternion.Euler(0, +90, 0);
-                animator.SetBool("walk", true);
-                break;
-            case (int)ACTIONS.BACKWARD:
-                transform.rotation = Quaternion.Euler(0, 180, 0);
-                animator.SetBool("walk", true);
-                break;
-        }
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInptu = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.forward * speed * Time.fixedDeltaTime);
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInptu);
+
+        float actionSpeed = (actionTaken[0] + 1) / 2;
+        float actionSteering = actionTaken[1]; 
+
+        transform.Translate(actionSpeed * transform.forward * speed * Time.fixedDeltaTime, Space.World);
+
+        transform.eulerAngles = new Vector3(
+                                   transform.eulerAngles.x,
+                                   transform.eulerAngles.y + actionSteering,
+                                   transform.eulerAngles.z
+                                           ); 
+
+        //transform.rotation = Quaternion.Euler(new Vector3(0, actionSteering, 0));
+
+        //if(movementDirection != Vector3.zero)
+        //{
+        //    //transform.forward = movementDirection;
+        //    Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+
+        //    transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        //}
 
         AddReward(-0.01f);
     }
